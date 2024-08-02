6356cad2d5d6ec79850d9b2c956f032263bc1fef
shuirs <evacheungevaeva@gmail.com>
2024-08-03 05:25:37 +0800

    Update toggl.py

--- scripts/toggl.py
+++ scripts/toggl.py
@@ -79,7 +79,7 @@ def insert_to_notion():
                         f"https://api.track.toggl.com/api/v9/workspaces/{workspace_id}/projects/{project_id}",
                         auth=auth,
                     )
-                    project = task.get("description")
+                    project = response.json().get("description")
                     emoji, project = split_emoji_from_string(project)
                     item["标题"] = project
                     client_id = response.json().get("cid")
